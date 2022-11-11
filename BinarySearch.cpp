ac72139f45505fb48c7a8af6ded388d7878f6796
rishavhav <rishavsoammw123@gmail.com>
2022-11-11 16:16:39 -0500

    prac 11/11

---
+++ BinarySearch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int key) {
+    int start =  0;
+    int end = size-1;
+    int mid = (start + end)/2;
+
+    while(start <= end) {
+        if(arr[mid]== key)
+            return mid;
+         if(arr[mid] < key ) {
+            start = mid + 1;
+         } else {
+            end = end - 1;
+         }
+
+         mid = (start + end)/2;
+    }
+    return -1;
+}
+
+int main() {
+    int even[6] = {2,3,67,200,324,999};
+    int odd[5] = {2,3,67,200,324};
+    cout << binarySearch(even, 6, 999);
+    return 0;
+}
