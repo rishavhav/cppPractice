1553ff821e481534a9182dabd7803890f3217cb8
rishavhav <rishavsoammw123@gmail.com>
2022-11-11 02:12:17 -0500

    practice11/11

--- revereseLeetcode.cpp
+++ revereseLeetcode.cpp
@@ -1,1 +1,24 @@
-caca
+#include<math.h>
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    cout << "enter the num";
+    int n, i = 0;
+    cin >> n;
+    bool isPositive = n > 0 ? true : false;
+    long long int rev = 0;
+    while(n) {
+        int x = n%10;
+        rev = 10*rev + x;
+        if(rev > INT_MAX || rev < INT_MIN)
+            return 0;
+        n = n/10;
+    }
+    
+
+    rev = isPositive ? rev : -rev;
+    cout << "reverse is: "<< rev;
+    return 0;
+}
