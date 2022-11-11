4c7b302dd2757a106421f2259d4075d015d60dcd
rishavhav <rishavsoammw123@gmail.com>
2022-11-11 16:20:25 -0500

    prac 11/11

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int binarySearch(int arr[], int size, int key) {
     int start =  0;
     int end = size-1;
-    int mid = (start + end)/2;
+    int mid = start + (end - start )/2;
 
     while(start <= end) {
         if(arr[mid]== key)
@@ -15,7 +15,7 @@ int binarySearch(int arr[], int size, int key) {
             end = end - 1;
          }
 
-         mid = (start + end)/2;
+         mid = start + (end - start )/2;
     }
     return -1;
 }
